8c46dbeecec0167d8d6f08313b5c595487bd35d4
AlperOzdamar <alper.ozdamar@gmail.com>
2019-11-03 01:50:47 -0800

    Constants should be public static final.

--- src/main/java/edu/usfca/cs/mr/constants/Constants.java
+++ src/main/java/edu/usfca/cs/mr/constants/Constants.java
@@ -2,6 +2,6 @@ package edu.usfca.cs.mr.constants;
 
 public class Constants {
 
-    public static String GEO_HASH_SANTA_BARBARA = "9q4g";
+    public static final String GEO_HASH_SANTA_BARBARA = "9q4g";
 
 }
