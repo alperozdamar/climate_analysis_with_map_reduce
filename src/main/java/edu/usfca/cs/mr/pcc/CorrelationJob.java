7282ca37ceae892712216c26557cd73279304809
Hiep Bui <phuhiep11395@outlook.com>
2019-11-08 15:50:07 -0800

    Add correlation problem code

---
+++ src/main/java/edu/usfca/cs/mr/pcc/CorrelationJob.java
@@ -0,0 +1,68 @@
+package edu.usfca.cs.mr.pcc;
+
+import edu.usfca.cs.mr.util.Utils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.File;
+
+public class CorrelationJob {
+    public static void main(String[] args) {
+        try {
+            Configuration conf = new Configuration();
+
+            //Delete existing output folder first.
+            Utils.deleteDirectory(new File(args[1]));
+
+            /* Job Name. You'll see this in the YARN webapp */
+            Job job = Job.getInstance(conf, "Hiep-Alper-Project2-Moving Out Job");
+
+            /* Current class */
+            job.setJarByClass(CorrelationJob.class);
+
+            /* Mapper class */
+            job.setMapperClass(CorrelationMapper.class);
+
+            /* Outputs from the Mapper. */
+            job.setMapOutputKeyClass(Text.class);
+            job.setMapOutputValueClass(RunningStatisticsND.class);
+
+            /* Combiner class. Combiners are run between the Map and Reduce
+             * phases to reduce the amount of output that must be transmitted.
+             * In some situations, we can actually use the Reducer as a Combiner
+             * but ONLY if its inputs and ouputs match up correctly. The
+             * combiner is disabled here, but the following can be uncommented
+             * for this particular job:
+            //job.setCombinerClass(WordCountReducer.class);
+
+            /* Reducer class */
+            job.setReducerClass(CorrelationReducer.class);
+
+            /* Outputs from the Reducer */
+            job.setOutputKeyClass(Text.class);
+            job.setOutputValueClass(RunningStatisticsND.class);
+
+            /* Reduce tasks */
+            job.setNumReduceTasks(1);
+
+            /* Job input path in HDFS */
+            FileInputFormat.addInputPath(job, new Path(args[0]));
+
+            /* Job output path in HDFS. NOTE: if the output path already exists
+             * and you try to create it, the job will fail. You may want to
+             * automate the creation of new output directories here */
+            FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+            /* Wait (block) for the job to complete... */
+            System.exit(job.waitForCompletion(true) ? 0 : 1);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/edu/usfca/cs/mr/pcc/CorrelationMapper.java
@@ -0,0 +1,39 @@
+package edu.usfca.cs.mr.pcc;
+
+import edu.usfca.cs.mr.constants.Constants;
+import edu.usfca.cs.mr.constants.NcdcConstants;
+import edu.usfca.cs.mr.util.Geohash;
+import edu.usfca.cs.mr.util.Utils;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class CorrelationMapper extends Mapper<LongWritable, Text, Text, RunningStatisticsND> {
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] values = value.toString().split("[ ]+");
+        double lat = Double.valueOf(values[NcdcConstants.LATITUDE]);
+        double lon = Double.valueOf(values[NcdcConstants.LONGITUDE]);
+        double temperature = Double.valueOf(values[NcdcConstants.AIR_TEMPERATURE]);
+        int humidity = Integer.valueOf(values[NcdcConstants.RELATIVE_HUMIDITY]);
+        String humidFlag = values[NcdcConstants.RH_FLAG];
+        int wetness = Integer.valueOf(values[NcdcConstants.WETNESS]);
+        String wetFlag = values[NcdcConstants.WET_FLAG];
+        double wind = Double.valueOf(values[NcdcConstants.WIND_1_5]);
+        String windFlag = values[NcdcConstants.WIND_FLAG];
+        if(!humidFlag.equals(NcdcConstants.QC_FLAG_GOOD) || !wetFlag.equals(NcdcConstants.QC_FLAG_GOOD)
+            || !windFlag.equals(NcdcConstants.QC_FLAG_GOOD)){
+            return;
+        }
+        if(Utils.isValidTemp(temperature) && Utils.isValidHumid(humidity) && Utils.isValidWetness(wetness)
+            && Utils.isValidWind(wind)){
+            String location = Geohash.encode(Float.valueOf(values[NcdcConstants.LATITUDE]),
+                    Float.valueOf(values[NcdcConstants.LONGITUDE]),
+                    Constants.GEO_HASH_PRECISION);
+            context.write(new Text(location), new RunningStatisticsND(new double[]{lat, lon, temperature, humidity, wetness, wind}));
+        }
+    }
+}
