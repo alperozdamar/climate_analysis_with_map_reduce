6650389261cc843ad780fa7d2d340b0d4c54eab6
AlperOzdamar <alper.ozdamar@gmail.com>
2019-11-10 18:35:44 -0800

    Exception handling for Movingout Mapper and reducer. It was failing to produce output with 13 years data.

--- src/main/java/edu/usfca/cs/mr/movingOut/MovingOutMapper.java
+++ src/main/java/edu/usfca/cs/mr/movingOut/MovingOutMapper.java
@@ -1,54 +1,64 @@
 package edu.usfca.cs.mr.movingOut;
 
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
 import edu.usfca.cs.mr.constants.Constants;
 import edu.usfca.cs.mr.constants.NcdcConstants;
 import edu.usfca.cs.mr.movingOut.models.ClimateWritable;
 import edu.usfca.cs.mr.movingOut.models.MonthLocationWritable;
 import edu.usfca.cs.mr.util.Geohash;
 import edu.usfca.cs.mr.util.Utils;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
 
-import java.io.IOException;
+public class MovingOutMapper
+        extends Mapper<LongWritable, Text, MonthLocationWritable, ClimateWritable> {
 
-public class MovingOutMapper extends Mapper<LongWritable, Text, MonthLocationWritable, ClimateWritable> {
     @Override
-    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        // tokenize into words.
-        String[] values = value.toString().split("[ ]+");
+    protected void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+        try {
+            // tokenize into words.
+            String[] values = value.toString().split("[ ]+");
 
-        float lat = Float.valueOf(values[NcdcConstants.LATITUDE]);
-        float lon = Float.valueOf(values[NcdcConstants.LONGITUDE]);
+            float lat = Float.valueOf(values[NcdcConstants.LATITUDE]);
+            float lon = Float.valueOf(values[NcdcConstants.LONGITUDE]);
 
-        double airTemp = Double.valueOf(values[NcdcConstants.AIR_TEMPERATURE]);
-//        double surfaceTemp = Double.valueOf(values[NcdcConstants.SURFACE_TEMPERATURE]);
-        int humidity = Integer.valueOf(values[NcdcConstants.RELATIVE_HUMIDITY]);
-//        int wetness = Integer.valueOf(values[NcdcConstants.WETNESS]);
+            double airTemp = Double.valueOf(values[NcdcConstants.AIR_TEMPERATURE]);
+            //        double surfaceTemp = Double.valueOf(values[NcdcConstants.SURFACE_TEMPERATURE]);
+            int humidity = Integer.valueOf(values[NcdcConstants.RELATIVE_HUMIDITY]);
+            //        int wetness = Integer.valueOf(values[NcdcConstants.WETNESS]);
 
-//        String sfFlag = values[NcdcConstants.ST_FLAG];
-//        String sfType = values[NcdcConstants.ST_TYPE];
-//        if (!sfType.equals(NcdcConstants.SF_TYPE_CORRECT) || !sfFlag.equals(NcdcConstants.QC_FLAG_GOOD)) {
-//            surfaceTemp = NcdcConstants.EXTREME_TEMP_LOW;
-//        }
+            //        String sfFlag = values[NcdcConstants.ST_FLAG];
+            //        String sfType = values[NcdcConstants.ST_TYPE];
+            //        if (!sfType.equals(NcdcConstants.SF_TYPE_CORRECT) || !sfFlag.equals(NcdcConstants.QC_FLAG_GOOD)) {
+            //            surfaceTemp = NcdcConstants.EXTREME_TEMP_LOW;
+            //        }
 
-        String humidityFlag = values[NcdcConstants.RH_FLAG];
-        if(!humidityFlag.equals(NcdcConstants.QC_FLAG_GOOD)){
-            humidity = NcdcConstants.EXTREME_HUMIDITY_LOW+1;
-        }
+            String humidityFlag = values[NcdcConstants.RH_FLAG];
+            if (!humidityFlag.equals(NcdcConstants.QC_FLAG_GOOD)) {
+                humidity = NcdcConstants.EXTREME_HUMIDITY_LOW + 1;
+            }
 
-//        String wetnessFlag = values[NcdcConstants.WET_FLAG];
-//        if(!wetnessFlag.equals(NcdcConstants.QC_FLAG_GOOD)){
-//            wetness = NcdcConstants.EXTREME_DRY;
-//        }
+            //        String wetnessFlag = values[NcdcConstants.WET_FLAG];
+            //        if(!wetnessFlag.equals(NcdcConstants.QC_FLAG_GOOD)){
+            //            wetness = NcdcConstants.EXTREME_DRY;
+            //        }
 
-        if(Utils.isValidTemp(airTemp) || Utils.isValidHumid(humidity)){
-            String dateString = values[NcdcConstants.UTC_DATE];
-            int month = Utils.getMonth(dateString);
-            String location = Geohash.encode(lat, lon, Constants.GEO_HASH_PRECISION);
-            MonthLocationWritable monthLocationWritable = new MonthLocationWritable(month, location);
-            ClimateWritable climateWritable = new ClimateWritable(airTemp, humidity);
-            context.write(monthLocationWritable, climateWritable);
+            if (Utils.isValidTemp(airTemp) || Utils.isValidHumid(humidity)) {
+                String dateString = values[NcdcConstants.UTC_DATE];
+                int month = Utils.getMonth(dateString);
+                String location = Geohash.encode(lat, lon, Constants.GEO_HASH_PRECISION);
+                MonthLocationWritable monthLocationWritable = new MonthLocationWritable(month,
+                                                                                        location);
+                ClimateWritable climateWritable = new ClimateWritable(airTemp, humidity);
+                context.write(monthLocationWritable, climateWritable);
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
 
     }
--- src/main/java/edu/usfca/cs/mr/movingOut/MovingOutReducer.java
+++ src/main/java/edu/usfca/cs/mr/movingOut/MovingOutReducer.java
@@ -1,68 +1,95 @@
 package edu.usfca.cs.mr.movingOut;
 
-import edu.usfca.cs.mr.constants.NcdcConstants;
-import edu.usfca.cs.mr.movingOut.models.OutputClimateWritable;
-import edu.usfca.cs.mr.movingOut.models.ClimateWritable;
-import edu.usfca.cs.mr.movingOut.models.MonthLocationWritable;
-import edu.usfca.cs.mr.util.Utils;
-import org.apache.hadoop.mapreduce.Reducer;
-
 import java.io.IOException;
 
-public class MovingOutReducer extends Reducer<MonthLocationWritable, ClimateWritable, MonthLocationWritable, OutputClimateWritable> {
+import org.apache.hadoop.mapreduce.Reducer;
+
+import edu.usfca.cs.mr.constants.NcdcConstants;
+import edu.usfca.cs.mr.movingOut.models.ClimateWritable;
+import edu.usfca.cs.mr.movingOut.models.MonthLocationWritable;
+import edu.usfca.cs.mr.movingOut.models.OutputClimateWritable;
+import edu.usfca.cs.mr.util.Utils;
+
+public class MovingOutReducer extends
+        Reducer<MonthLocationWritable, ClimateWritable, MonthLocationWritable, OutputClimateWritable> {
 
     @Override
-    protected void reduce(MonthLocationWritable key, Iterable<ClimateWritable> values, Context context) throws IOException, InterruptedException {
-        double minAirTemp = Double.MAX_VALUE;
-        double maxAirTemp = Double.NEGATIVE_INFINITY;
-        double avgAirTemp = 0;
-        int countAirTemp = 0;
-//        double avgSurfaceTemp = 0;
-//        int countSurfaceTemp = 0;
-        double avgHumidity = 0;
-        int countAvgHumidity = 0;
-//        double avgWetness = 0;
-//        int countAvgWetness = 0;
-        for(ClimateWritable value : values){
-            double airTemp = value.getAirTemp().get();
-            if(Utils.isValidTemp(airTemp)){
-                countAirTemp++;
-                avgAirTemp += (airTemp - avgAirTemp) / countAirTemp;
-                if(airTemp < minAirTemp){
-                    minAirTemp = airTemp;
+    protected void reduce(MonthLocationWritable key, Iterable<ClimateWritable> values,
+                          Context context)
+            throws IOException, InterruptedException {
+        try {
+            double minAirTemp = Double.MAX_VALUE;
+            double maxAirTemp = Double.NEGATIVE_INFINITY;
+            double avgAirTemp = 0;
+            int countAirTemp = 0;
+            //        double avgSurfaceTemp = 0;
+            //        int countSurfaceTemp = 0;
+            double avgHumidity = 0;
+            int countAvgHumidity = 0;
+            //        double avgWetness = 0;
+            //        int countAvgWetness = 0;
+            for (ClimateWritable value : values) {
+                double airTemp = value.getAirTemp().get();
+                if (Utils.isValidTemp(airTemp)) {
+                    countAirTemp++;
+                    avgAirTemp += (airTemp - avgAirTemp) / countAirTemp;
+                    if (airTemp < minAirTemp) {
+                        minAirTemp = airTemp;
+                    }
+                    if (airTemp > maxAirTemp) {
+                        maxAirTemp = airTemp;
+                    }
                 }
-                if(airTemp > maxAirTemp){
-                    maxAirTemp = airTemp;
+                //            if(Utils.isValidTemp(value.getDiffTemp().get())){
+                //                countSurfaceTemp++;
+                //                avgSurfaceTemp += (value.getDiffTemp().get() - avgSurfaceTemp) / countSurfaceTemp;
+                //            }
+                int humid = value.getHumidity().get();
+                if (Utils.isValidHumid(humid)) {
+                    countAvgHumidity++;
+                    avgHumidity += (value.getHumidity().get() - avgHumidity) / countAvgHumidity;
                 }
+                //            if(Utils.isValidWetness(value.getDiffHumid().get())){
+                //                countAvgWetness++;
+                //                avgWetness += (value.getDiffHumid().get() - avgWetness) / countAvgWetness;
+                //            }
             }
-//            if(Utils.isValidTemp(value.getDiffTemp().get())){
-//                countSurfaceTemp++;
-//                avgSurfaceTemp += (value.getDiffTemp().get() - avgSurfaceTemp) / countSurfaceTemp;
-//            }
-            int humid = value.getHumidity().get();
-            if(Utils.isValidHumid(humid)){
-                countAvgHumidity++;
-                avgHumidity += (value.getHumidity().get() - avgHumidity) / countAvgHumidity;
+            int month = key.getMonth().get();
+            avgAirTemp = countAirTemp == 0 ? NcdcConstants.EXTREME_TEMP_LOW
+                    : Math.round(avgAirTemp * 100.0) / 100.0;
+            //        avgSurfaceTemp = countSurfaceTemp==0 ? NcdcConstants.EXTREME_TEMP_LOW : Math.round(avgSurfaceTemp * 100.0) / 100.0;
+            avgHumidity = countAvgHumidity == 0 ? NcdcConstants.EXTREME_HUMIDITY_LOW
+                    : Math.round(avgHumidity * 100.0) / 100.0;
+            //        avgWetness = countAvgWetness==0 ? NcdcConstants.EXTREME_DRY : Math.round(avgWetness * 100.0) / 100.0;
+            if (avgAirTemp != NcdcConstants.EXTREME_TEMP_LOW
+                    && avgHumidity != NcdcConstants.EXTREME_HUMIDITY_LOW) {
+                //            minAirTemp = Math.round(minAirTemp * 100.0) / 100.0;
+                //            maxAirTemp = Math.round(maxAirTemp * 100.0) / 100.0;
+                context.write(key,
+                              new OutputClimateWritable(minAirTemp,
+                                                        Math.round(Math
+                                                                .abs(MovingOutConfig.minTemp[month]
+                                                                        - minAirTemp)
+                                                                * 100.0) / 100.0,
+                                                        maxAirTemp,
+                                                        Math.round(Math
+                                                                .abs(MovingOutConfig.maxTemp[month]
+                                                                        - maxAirTemp)
+                                                                * 100.0) / 100.0,
+                                                        avgAirTemp,
+                                                        Math.round(Math
+                                                                .abs(MovingOutConfig.avgTemp[month]
+                                                                        - avgAirTemp)
+                                                                * 100.0) / 100.0,
+                                                        avgHumidity,
+                                                        Math.round(Math
+                                                                .abs(MovingOutConfig.avgHumid[month]
+                                                                        - avgHumidity)
+                                                                * 100.0) / 100.0));
             }
-//            if(Utils.isValidWetness(value.getDiffHumid().get())){
-//                countAvgWetness++;
-//                avgWetness += (value.getDiffHumid().get() - avgWetness) / countAvgWetness;
-//            }
-        }
-        int month = key.getMonth().get();
-        avgAirTemp = countAirTemp==0 ? NcdcConstants.EXTREME_TEMP_LOW : Math.round(avgAirTemp * 100.0) / 100.0;
-//        avgSurfaceTemp = countSurfaceTemp==0 ? NcdcConstants.EXTREME_TEMP_LOW : Math.round(avgSurfaceTemp * 100.0) / 100.0;
-        avgHumidity = countAvgHumidity==0 ? NcdcConstants.EXTREME_HUMIDITY_LOW : Math.round(avgHumidity * 100.0) / 100.0;
-//        avgWetness = countAvgWetness==0 ? NcdcConstants.EXTREME_DRY : Math.round(avgWetness * 100.0) / 100.0;
-        if(avgAirTemp != NcdcConstants.EXTREME_TEMP_LOW && avgHumidity != NcdcConstants.EXTREME_HUMIDITY_LOW) {
-//            minAirTemp = Math.round(minAirTemp * 100.0) / 100.0;
-//            maxAirTemp = Math.round(maxAirTemp * 100.0) / 100.0;
-            context.write(key, new OutputClimateWritable(
-                    minAirTemp, Math.round(Math.abs(MovingOutConfig.minTemp[month] - minAirTemp) * 100.0) / 100.0,
-                    maxAirTemp, Math.round(Math.abs(MovingOutConfig.maxTemp[month] - maxAirTemp) * 100.0) / 100.0,
-                    avgAirTemp, Math.round(Math.abs(MovingOutConfig.avgTemp[month] - avgAirTemp) * 100.0) / 100.0,
-                    avgHumidity, Math.round(Math.abs(MovingOutConfig.avgHumid[month] - avgHumidity) * 100.0) / 100.0)
-            );
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 }
